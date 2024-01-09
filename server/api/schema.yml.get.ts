d2cb93b573b722d5d69bbe321e17b4623ff20624
Tom Sawyer <t.sawyer@riseup.net>
2024-01-09 04:40:44 +0100

    filters

--- server/api/schema.yml.get.ts
+++ server/api/schema.yml.get.ts
@@ -2,5 +2,5 @@ import { readFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 
 export default defineEventHandler(() => {
-  return readFileSync(resolve('./content/_meta.yml')).toString()
+  return readFileSync(resolve('./content/schema.yml')).toString()
 })
