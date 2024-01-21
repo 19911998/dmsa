06d22954707ed85e2ba481cfe0f03c33a42a6aaf
Tom Sawyer <t.sawyer@riseup.net>
2024-01-21 03:48:53 +0100

    contact form

---
+++ components/ContactForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <UModal
+    :prevent-close="Object.values(state).some(Boolean)"
+    :model-value="modelValue"
+    @update:model-value="$emit('update:model-value', $event)"
+  >
+    <div class="p-4">
+      <div class="flex items-center justify-between pb-4">
+        <h3 class="text-2xl font-semibold leading-6 text-gray-900 dark:text-white">
+          Kontaktformular
+        </h3>
+        <UButton
+          color="gray"
+          variant="ghost"
+          icon="i-heroicons-x-mark-20-solid"
+          size="xl"
+          class="-my-1"
+          @click="$emit('update:model-value', false)"
+        />
+      </div>
+
+      <UForm :state="state" class="space-y-4" netlify>
+        <UFormGroup label="Name" name="name">
+          <UInput v-model="state.name" type="password" />
+        </UFormGroup>
+
+        <UFormGroup label="Email" name="email">
+          <UInput v-model="state.email" />
+        </UFormGroup>
+
+        <UFormGroup label="Nachricht" name="message">
+          <UTextarea v-model="state.message" autoresize />
+        </UFormGroup>
+
+        <div class="text-right">
+          <UButton type="submit">
+            Mail senden
+          </UButton>
+        </div>
+      </UForm>
+    </div>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true
+  }
+})
+
+defineEmits(['update:model-value'])
+
+const state = reactive({
+  email: '',
+  name: '',
+  message: ''
+})
+</script>
--- components/Footer.vue
+++ components/Footer.vue
@@ -14,10 +14,26 @@
           aria-label="RSS Feed"
         />
       </UTooltip>
+
+      <ContactForm v-model="openContactForm" />
+
+      <UTooltip text="Kontaktformular" placement="top">
+        <UButton
+          icon="i-heroicons-envelope"
+          variant="ghost"
+          color="gray"
+          aria-label="Kontaktformular"
+          @click="openContactForm = true"
+        />
+      </UTooltip>
     </template>
   </UFooter>
 </template>
 
 <script setup lang="ts">
+import ContactForm from '~/components/ContactForm.vue'
+
 const { footer } = useAppConfig()
+
+const openContactForm = ref(false)
 </script>
